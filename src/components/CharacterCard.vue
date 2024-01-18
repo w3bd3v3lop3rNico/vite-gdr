124f9030434d3461b6d0980bfa3723f1581444f8
aSwanting <gab.damico@gmail.com>
2024-01-18 17:54:26 +0100

    add: character card style

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="character-card">
+    <div>
+      <div class="card-text name">{{ character.name }}</div>
+      <div class="card-text character-class">{{ character.type.name }}</div>
+    </div>
+
+    <div class="card-text bio">{{ character.bio }}</div>
+
+    <div class="card-stats">
+      <div class="card-text hp">
+        <span class="stats">HP </span>{{ character.hp }}
+      </div>
+      <div class="card-text atk">
+        <span class="stats">ATK </span>{{ character.attack }}
+      </div>
+      <div class="card-text def">
+        <span class="stats">DEF </span>{{ character.defense }}
+      </div>
+      <div class="card-text spd">
+        <span class="stats">SPD </span>{{ character.speed }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    character: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.character-card {
+  background: url(../../public/card-bg.jpg);
+  background-size: cover;
+  background-color: rgb(62, 62, 75);
+  color: rgba(255, 255, 255, 0.9);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 30px;
+
+  overflow: hidden;
+  padding: 50px 30px;
+
+  box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.25);
+  text-shadow: 3px 3px 8px rgba(0, 0, 0, 0.2);
+  outline: 0px solid;
+  transition: 100ms all;
+
+  &:hover {
+    outline: 6px solid rgba(105, 255, 143, 0.8);
+    box-shadow: 0px 0px 13px rgba(0, 0, 0, 0.25);
+  }
+
+  .card-stats {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    // justify-content: space-evenly;
+    // flex-grow: 1;
+    text-align: center;
+    font-size: 20px;
+    // font-weight: 300;
+
+    .card-text {
+      position: relative;
+      width: 60%;
+      margin: 0 auto;
+
+      .stats {
+        position: absolute;
+        top: 4px;
+        left: 0;
+        font-size: 12px;
+        font-weight: 700;
+        margin-right: 10px;
+      }
+    }
+
+    // .card-text::after {
+    //   content: "";
+    //   display: block;
+    //   width: 100%;
+    //   margin-block: 10px;
+    //   height: 1px;
+    //   background-color: rgba(255, 255, 255, 0.3);
+    // }
+
+    // .card-text:last-child::after {
+    //   display: none;
+    // }
+  }
+
+  .card-text {
+    // border: 1px solid;
+    &.name {
+      text-transform: uppercase;
+      font-size: 30px;
+      //   text-align: center;
+      //   min-height: 2.5em;
+      //   display: flex;
+      //   justify-content: center;
+      //   align-items: center;
+      //   font-weight: 700;
+      //   line-height: 1.2em;
+    }
+
+    &.character-class {
+      font-size: 18px;
+    }
+
+    &.bio {
+      border-left: 2px solid rgba(255, 255, 255, 0.3);
+      padding: 0px 10px;
+      min-height: 50px;
+      max-height: 100px;
+      overflow: auto;
+    }
+  }
+}
+
+::-webkit-scrollbar {
+  width: 2px;
+  background-color: rgba(0, 0, 0, 0);
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: rgba(255, 255, 255, 0.3);
+}
+</style>
--- src/pages/characters/Index.vue
+++ src/pages/characters/Index.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="section">
     <div class="container">
-      <ul v-for="character in characters">
+      <div class="grid-wrapper">
+        <CharacterCard v-for="character in characters" :character="character" />
+      </div>
+      <!-- <ul v-for="character in characters">
         <li>{{ character.name }}</li>
         <li>{{ character.type.name }}</li>
         <li>{{ character.bio }}</li>
@@ -14,14 +17,18 @@
         <ul>
           <li v-for="item in character.items">{{ item.name }}</li>
         </ul>
-      </ul>
+      </ul> -->
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import CharacterCard from "../../components/CharacterCard.vue";
 export default {
+  components: {
+    CharacterCard,
+  },
   data() {
     return {
       characters: [],
@@ -42,4 +49,11 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.grid-wrapper {
+  margin: 60px 20px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 30px;
+}
+</style>

ddee15306fe9a3c953225e06d1b785a0c55efd3a
Nicola Tabai <nicola.tabai98@gmail.com>
2024-01-18 17:23:36 +0100

    Add Header component

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,47 @@
+<template>
+    <nav class="container">
+        <ul class="nav-body">
+            <li class="nav-item home">
+                <RouterLink :to="{ name: 'home' }">
+                    Home
+                </RouterLink>
+            </li>
+            <li class="nav-item characters">
+                <RouterLink :to="{ name: 'characters' }">
+                    Characters
+                </RouterLink>
+            </li>
+        </ul>
+    </nav>
+</template>
+<script>
+import { router } from '../router';
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+.container {
+    height: 40px;
+    .nav-body {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        gap: 8px;
+
+        .nav-item {
+            background-color: red;
+            color: white;
+            padding: 5px;
+            border-radius: 20px;
+
+            :hover {
+                font-size: 20px;
+                color: black;
+            }
+        }
+    }
+}
+</style>

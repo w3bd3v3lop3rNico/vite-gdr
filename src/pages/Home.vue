4b3271c04511b3a06d737411f200ae6b142932e3
aSwanting <gab.damico@gmail.com>
2024-01-18 16:12:23 +0100

    add: fresh vite install, router, home.vue

---
+++ src/pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <section>
+      <h1>WELCOME HOME</h1>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
